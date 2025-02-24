6287ef89bddd78777a0fc8b3ffce8095e0e1d564
josip <josip@laptop>
2025-02-24 18:10:32 +0100

    update firmware

--- src/Zadatak2/Main.java
+++ src/Zadatak2/Main.java
@@ -13,15 +13,18 @@ Ažurirajte firmver svakog uređaja i prikažite ažurirani status.*/
         termostat.performAction("25");
         termostat.performAction("Show");
         termostat.remoteControl("26");
+        termostat.updateFirmware("2.1");
         termostat.displayStatus();
         speaker.displayStatus();
         speaker.performAction("On");
         speaker.performAction("+");
         speaker.remoteControl("+");
+        speaker.updateFirmware("3.3");
         speaker.displayStatus();
         light.displayStatus();
         light.performAction("On");
         light.performAction("-");
+        light.updateFirmware("1.1");
         light.remoteControl("-");
         light.displayStatus();
     }
